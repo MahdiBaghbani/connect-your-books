4d483b434968aba0c6b488c93dcbab235ac962ed
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2023-05-10 14:35:51 +0330

    update: salvo version to 0.41.0

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,16 +1,15 @@
 use std::env;
 
-use salvo::compression::{Compression, CompressionAlgo};
-use salvo::listener::TcpListener;
-use salvo::logging::Logger;
+use salvo::compression::Compression;
+use salvo::conn::tcp::TcpAcceptor;
+use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
-use salvo::{Router, Server};
 
 #[tokio::main]
 pub async fn run() {
     let host: String = env::var("CYB_HOST").expect("CYB_HOST is not set in .env file");
     let port: String = env::var("CYB_PORT").expect("CYB_PORT is not set in .env file");
-    let server_url: String = format!("{host}:{port}");
+    let url: String = format!("{host}:{port}");
 
     let _db_type: String = env::var("CYB_DB_TYPE").expect("CYB_DB_TYPE is not set in .env file");
     let _db_host: String = env::var("CYB_DB_HOST").expect("CYB_DB_HOST is not set in .env file");
@@ -19,20 +18,25 @@ pub async fn run() {
     let _db_user: String = env::var("CYB_DB_USER").expect("CYB_DB_USER is not set in .env file");
     let _db_pass: String = env::var("CYB_DB_PASS").expect("CYB_DB_PASS is not set in .env file");
 
-    let frontend_directory: String =
-        env::var("CYB_FRONTEND_DIRECTORY").expect("CYB_FRONTEND_DIRECTORY is not set in .env file");
+    let frontend: String = env::var("CYB_FRONTEND").expect("CYB_FRONTEND is not set in .env file");
+
+    let logger: Logger = Logger::new();
+
+    let compression: Compression = Compression::new()
+        .disable_all()
+        .enable_brotli(CompressionLevel::Minsize)
+        .force_priority(true);
 
     let router: Router = Router::new()
-        .hoop(Logger)
-        .hoop(Compression::new().with_algos(&[CompressionAlgo::Brotli]))
+        .hoop(logger)
+        .hoop(compression)
         .path("<**path>")
         .get(
-            StaticDir::new([frontend_directory])
-                .with_defaults("index.html")
-                .with_listing(true),
+            StaticDir::new([frontend])
+                .defaults("index.html")
+                .listing(true),
         );
 
-    let acceptor: TcpListener = TcpListener::bind(&server_url);
-    tracing::info!("Listening on {server_url}");
+    let acceptor: TcpAcceptor = TcpListener::new(url).bind().await;
     Server::new(acceptor).serve(router).await;
 }
