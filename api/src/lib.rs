d5f5abd0fa6986dde14d7e107ae63cfa1d11454c
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2023-07-10 13:32:48 +0330

    add: openapi docs

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,8 +2,18 @@ use std::env;
 
 use salvo::compression::Compression;
 use salvo::conn::tcp::TcpAcceptor;
+use salvo::cors::Cors;
+use salvo::http::Method;
+use salvo::oapi::extract::*;
 use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
+
+mod users;
+
+#[endpoint]
+async fn hello(name: QueryParam<String, false>) -> String {
+    format!("Hello, {}!", name.as_deref().unwrap_or("World"))
+}
 
 #[tokio::main]
 pub async fn run() {
@@ -20,22 +30,48 @@ pub async fn run() {
 
     let frontend: String = env::var("CYB_FRONTEND").expect("CYB_FRONTEND is not set in .env file");
 
+    // create necessary middlewares.
     let logger: Logger = Logger::new();
+
+    let cache: CachingHeaders = CachingHeaders::new();
 
     let compression: Compression = Compression::new()
         .disable_all()
         .enable_brotli(CompressionLevel::Minsize)
         .force_priority(true);
 
+    let cors_handler = Cors::new()
+        .allow_origin(&url)
+        .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
+        .into_handler();
+
+    // create base router with all the middlewares.
     let router: Router = Router::new()
         .hoop(logger)
+        .hoop(cache)
         .hoop(compression)
-        .path("<**path>")
-        .get(
+        .hoop(cors_handler)
+        .options(handler::empty());
+
+    // push paths into the basic router.
+    let router: Router = router.push(Router::new().path("hello").get(hello));
+
+    // create api documentation from routes.
+    let doc: OpenApi = OpenApi::new("Connect Your Books API", "0.0.1").merge_router(&router);
+
+    // push api documentation and swagger into router.
+    let router: Router = router
+        .push(doc.into_router("/api/doc/openapi.json"))
+        .push(SwaggerUi::new("/api/doc/openapi.json").into_router("swagger-ui"));
+
+    // push frontend static files to root of url.
+    let router: Router = router.push(
+        Router::new().path("<**rest_path>").get(
             StaticDir::new([frontend])
                 .defaults("index.html")
                 .listing(true),
-        );
+        ),
+    );
 
     let acceptor: TcpAcceptor = TcpListener::new(url).bind().await;
     Server::new(acceptor).serve(router).await;
