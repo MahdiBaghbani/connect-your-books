7033192b36770c3c66b4a9b8dbe0fc56d79b2509
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2023-08-13 10:55:30 +0330

    modify: use config struct.

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use crate::config::Config;
 use salvo::compression::Compression;
 use salvo::conn::tcp::TcpAcceptor;
 use salvo::cors::Cors;
@@ -7,6 +8,7 @@ use salvo::http::Method;
 use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
 
+mod config;
 mod users;
 
 fn api_router() -> Router {
@@ -15,18 +17,8 @@ fn api_router() -> Router {
 
 #[tokio::main]
 pub async fn run() {
-    let host: String = env::var("CYB_HOST").expect("CYB_HOST is not set in .env file");
-    let port: String = env::var("CYB_PORT").expect("CYB_PORT is not set in .env file");
-    let url: String = format!("{host}:{port}");
-
-    let _db_type: String = env::var("CYB_DB_TYPE").expect("CYB_DB_TYPE is not set in .env file");
-    let _db_host: String = env::var("CYB_DB_HOST").expect("CYB_DB_HOST is not set in .env file");
-    let _db_port: String = env::var("CYB_DB_PORT").expect("CYB_DB_PORT is not set in .env file");
-    let _db_name: String = env::var("CYB_DB_NAME").expect("CYB_DB_NAME is not set in .env file");
-    let _db_user: String = env::var("CYB_DB_USER").expect("CYB_DB_USER is not set in .env file");
-    let _db_pass: String = env::var("CYB_DB_PASS").expect("CYB_DB_PASS is not set in .env file");
-
-    let frontend: String = env::var("CYB_FRONTEND").expect("CYB_FRONTEND is not set in .env file");
+    // load application configuration from .env file.
+    let config: Config = Config::new();
 
     // create necessary middlewares.
     let logger: Logger = Logger::new();
@@ -39,7 +31,7 @@ pub async fn run() {
         .force_priority(true);
 
     let cors_handler = Cors::new()
-        .allow_origin(&url)
+        .allow_origin(&config.url())
         .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
         .into_handler();
 
@@ -65,12 +57,12 @@ pub async fn run() {
     // push frontend static files to root of url.
     let router: Router = router.push(
         Router::new().path("<**rest_path>").get(
-            StaticDir::new([frontend])
+            StaticDir::new([config.frontend.clone()])
                 .defaults("index.html")
                 .listing(true),
         ),
     );
 
-    let acceptor: TcpAcceptor = TcpListener::new(url).bind().await;
+    let acceptor: TcpAcceptor = TcpListener::new(config.url()).bind().await;
     Server::new(acceptor).serve(router).await;
 }
