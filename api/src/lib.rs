4048f1c150b8b1ed917d9654e2c699aa4a5363c6
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2023-08-19 14:20:39 +0330

    add: database setup function

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,39 +1,35 @@
 use salvo::conn::tcp::TcpAcceptor;
 use salvo::prelude::*;
 
-use database::sea_orm::{Database, DatabaseConnection};
-use migration::{Migrator, MigratorTrait};
-
 mod configs;
+mod databases;
 mod middlewares;
 mod routers;
 mod schemas;
 mod users;
 
 use configs::Config;
+use database::sea_orm::DatabaseConnection;
 use schemas::AppState;
 
 #[tokio::main]
 pub async fn run() {
     // load application configuration from .env file.
-    let config: Config = Config::new();
+    let configs: Config = Config::new();
 
-    // connect to database.
-    let db_connection: DatabaseConnection =
-        Database::connect(&config.url_database()).await.unwrap();
-    // apply all pending migrations.
-    Migrator::up(&db_connection, None).await.unwrap();
+    // setup database.
+    let db_connection: DatabaseConnection = databases::setup_database(&configs).await;
 
     // global app state.
     let app_state: AppState = AppState { db_connection };
 
     // setup all required middlewares.
-    let middlewares: Router = middlewares::setup(&config, app_state);
+    let middlewares: Router = middlewares::setup(&configs, app_state);
 
     // setup api router with all middlewares and static assets.
-    let router: Router = routers::setup(&config, middlewares);
+    let router: Router = routers::setup(&configs, middlewares);
 
     // start server with router.
-    let acceptor: TcpAcceptor = TcpListener::new(config.url_api()).bind().await;
+    let acceptor: TcpAcceptor = TcpListener::new(configs.url_api()).bind().await;
     Server::new(acceptor).serve(router).await;
 }
