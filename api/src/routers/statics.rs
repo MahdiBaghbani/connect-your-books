5130f7e1d2cc87130a26f75d16fda930d48df827
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2023-12-01 15:14:38 +0330

    fix: deprecated method
    Signed-off-by: Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>

--- api/src/routers/statics.rs
+++ api/src/routers/statics.rs
@@ -2,7 +2,7 @@ use salvo::prelude::*;
 use salvo::serve_static::StaticDir;
 
 pub fn setup_frontend(path: String) -> Router {
-    let frontend: StaticDir = StaticDir::new([path]).defaults("index.html").listing(true);
+    let frontend: StaticDir = StaticDir::new([path]).defaults("index.html").auto_list(true);
 
     Router::new().path("<**rest_path>").get(frontend)
 }
