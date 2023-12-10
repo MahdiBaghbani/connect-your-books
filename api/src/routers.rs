fd273dec6e175d5767537fe38946ca5a7e0fbbc2
Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>
2023-12-10 19:53:18 +0330

    add: openapi json and frameworks
    Signed-off-by: Mahdi Baghbani <mahdi-baghbani@azadehafzar.io>

--- api/src/routers.rs
+++ api/src/routers.rs
@@ -1,7 +1,8 @@
 use salvo::Router;
 
-use docs::{setup_openapi, setup_swagger_ui};
-
+use crate::routers::docs::{
+    setup_open_api, setup_rapidoc, setup_redoc, setup_scalar, setup_swagger,
+};
 use crate::schemas::AppState;
 
 mod docs;
@@ -11,16 +12,56 @@ mod v1;
 
 pub fn setup(app_state: &AppState, middlewares: Router) -> Router {
     // merge all routers into one router.
-    let mut router: Router = Router::new()
-        .path("api")
-        .push(healthcheck::setup())
-        .push(v1::setup());
+    let mut router: Router = Router::new().path("api").push(healthcheck::setup());
 
     router = middlewares.push(router);
-    // generate openapi.json.
-    router = setup_openapi(router);
-    // enable swagger ui.
-    router = setup_swagger_ui(router);
+
+    if app_state.configs.open_api.enabled {
+        // generate openapi.json.
+        let open_api_path: String = app_state.configs.open_api.url_path.clone();
+        router = setup_open_api(
+            router,
+            &open_api_path,
+            "Connect Your Books OpenAPI Document.",
+            "v0.0.1",
+        );
+
+        if app_state.configs.open_api.swagger.enabled {
+            // enable swagger ui.
+            router = setup_swagger(
+                router,
+                app_state.configs.open_api.swagger.url_path.clone(),
+                open_api_path.clone(),
+            );
+        }
+
+        if app_state.configs.open_api.scalar.enabled {
+            // enable scalar.
+            router = setup_scalar(
+                router,
+                app_state.configs.open_api.scalar.url_path.clone(),
+                open_api_path.clone(),
+            );
+        }
+
+        if app_state.configs.open_api.rapi_doc.enabled {
+            // enable rapidoc.
+            router = setup_rapidoc(
+                router,
+                app_state.configs.open_api.rapi_doc.url_path.clone(),
+                open_api_path.clone(),
+            );
+        }
+
+        if app_state.configs.open_api.re_doc.enabled {
+            // enable redoc.
+            router = setup_redoc(
+                router,
+                app_state.configs.open_api.re_doc.url_path.clone(),
+                open_api_path.clone(),
+            );
+        }
+    }
 
     // push static assets into routers.
     router = router.push(statics::setup_frontend(app_state.configs.frontend.clone()));
